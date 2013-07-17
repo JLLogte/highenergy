9480626ae9c3acb961a74a6577337c0a3ce71b24
rootwizzy <zack@blzj.com>
2013-07-17 13:41:16 -0700

    Changed root route to be welcome#index to show subscribed articles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+
   def index
+  	if user_signed_in?
+      @articles = Array.new
+  	 Game.all.each do |game|
+  	   @articles << game.articles if current_user.is_subscribed?(game)
+      end
+      @articles.flatten!
+  	end
   end
+
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    
     if user.is_admin?
       can :manage, :all
     else
       can :read, :all
     end
+
   end
 end
